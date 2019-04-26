a04c330c21dc48d2d33feb412dff028a6b02d7ca
João Henrique Machado Silva <joao.henrique-silva@dazn.com>
2019-04-26 11:54:40 -0300

    nth fibonacci recursive and dynamic algorithms. Recursive is way too slow.

--- chapter_3/exercises/nth_fibonacci/src/main.rs
+++ chapter_3/exercises/nth_fibonacci/src/main.rs
@@ -1,12 +1,27 @@
 fn main() {
-    print!("Result: {}\n", nth_fibonacci(10)); 
+    print!("Result - recursive: {}\n", nth_fibonacci_recursive(50)); 
     // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+
+    print!("Result - dynamic: {}\n", nth_fibonacci_dynamic(50));
 }
 
-fn nth_fibonacci(n: i32) -> i32 {
+fn nth_fibonacci_recursive(n: i32) -> u64 {
     if n <= 2 {
         1 
     } else {
-        nth_fibonacci(n-1) + nth_fibonacci(n-2)
+        nth_fibonacci_recursive(n-1) + nth_fibonacci_recursive(n-2)
     }
+}
+
+fn nth_fibonacci_dynamic(n: usize) -> u64 {
+    let mut f: Vec<u64> = Vec::with_capacity(n+1);
+    f.push(1);
+    f.push(1);
+    let mut index = 2;
+    while index < n {
+        let push = f[index-1].wrapping_add(f[index-2]);
+        f.push(push);
+        index = index + 1;
+    }
+    f[n-1]
 }
